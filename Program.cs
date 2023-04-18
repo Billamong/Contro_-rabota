d33e5450170ce089033fd361e9c85f735d6e62a1
Denis <Dnik1993@yandex.ru>
2023-04-18 19:50:15 +0300

    Added arrays

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// string[] array (string[] args)
+// {
+//     // Инициализация массива строк
+//     string[] daysOfWeek = { "Sunday", "Monday", "Tuersday",
+//       "Wednesday", "Thirsday", "Friday", "Saturday" };
+
+//     // Вывод массива строк AS в цикле
+//     for (int i = 0; i < args.Length; i++)
+//         Console.WriteLine("AS[{0}] = {1}", i, daysOfWeek[i]);
+//     Console.ReadKey();
+// }
+
 Console.Clear();
+string[] massiv = { "Sunday", "Monday", "Tu",
+      "Wednesday", "Thi", "Friday", "Saturday" };
+string[] array = new string[10];
+int j =0;
+for (int i = 0; i < massiv.Length; i++)
+{
+    Console.Write($"{massiv[i]}, ");
+        if (massiv[i].Length <= 3){
+            array[j] += massiv[i];
+            j++;
+        }
+}
+
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++){
+    if(array !=0)
+        Console.Write($"{array[i]}, ");
+}
+
